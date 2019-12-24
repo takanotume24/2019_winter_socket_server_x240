7f6068730baddfaa4c545c337516cbd46ac5a089
Takuya MATSUNAGA <takanotume24@gmail.com>
2019-12-24 12:55:58 +0900

    1224

--- socket_server_no_openvino.py
+++ socket_server_no_openvino.py
@@ -68,36 +68,36 @@ if __name__ == "__main__":
         frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
         frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
-        if frame is None:
-            raise Exception('Image not found!')
+        #if frame is None:
+       #     raise Exception('Image not found!')
         # Prepare input blob and perform an inference.
-        blob = cv2.dnn.blobFromImage(frame, size=(672, 384), ddepth=cv2.CV_8U)
-        net.setInput(blob)
-        out = net.forward()
+       # blob = cv2.dnn.blobFromImage(frame, size=(672, 384), ddepth=cv2.CV_8U)
+       # net.setInput(blob)
+       # out = net.forward()
 
-        max_area = 0
-        faces = out.reshape(-1, 7)
+        #max_area = 0
+        # faces = out.reshape(-1, 7)
         # Draw detected faces on the frame.
-        for detection in faces:
-            confidence = float(detection[2])
-            xmin = int(detection[3] * frame.shape[1])
-            ymin = int(detection[4] * frame.shape[0])
-            xmax = int(detection[5] * frame.shape[1])
-            ymax = int(detection[6] * frame.shape[0])
-            if confidence > 0.5:
-                cv2.rectangle(frame, (xmin, ymin),
-                              (xmax, ymax), color=(0, 255, 0))
-                area = (xmax - xmin) * (ymax - ymin)
-                if area > max_area:
-                    max_area = area
-                    xmin_to_send = xmin
-                    xmax_to_send = xmax
+        #for detection in faces:
+        #    confidence = float(detection[2])
+        #    xmin = int(detection[3] * frame.shape[1])
+        #    ymin = int(detection[4] * frame.shape[0])
+        #    xmax = int(detection[5] * frame.shape[1])
+        #    ymax = int(detection[6] * frame.shape[0])
+        #    if confidence > 0.5:
+        #        cv2.rectangle(frame, (xmin, ymin),
+        #                      (xmax, ymax), color=(0, 255, 0))
+        #        area = (xmax - xmin) * (ymax - ymin)
+        #        if area > max_area:
+        #            max_area = area
+        #            xmin_to_send = xmin
+        #            xmax_to_send = xmax
 
-        if(max_area > 0):
-            x_center = calc_x_center(xmin_to_send, xmax_to_send)
-        else:
-            x_center = 0
-
+        #if(max_area > 0):
+        #    x_center = calc_x_center(xmin_to_send, xmax_to_send)
+        #else:
+        #    x_center = 0
+        x_center = 0
         print(x_center)
 
         conn_motor.sendall(str(x_center).zfill(4).encode())
@@ -106,6 +106,6 @@ if __name__ == "__main__":
         fliped_frame = cv2.flip(frame, 1)
         # resized_frame = cv2.resize(fliped_frame, (frame.shape[1] * 2, frame.shape[0] * 2))
         cv2.namedWindow("camera", cv2.WINDOW_KEEPRATIO | cv2.WINDOW_NORMAL)
-        cv2.imshow("camera", fliped_frame)
+        cv2.imshow("camera",fliped_frame)
         conn_image.sendall(b'ok')
         cv2.waitKey(1)
